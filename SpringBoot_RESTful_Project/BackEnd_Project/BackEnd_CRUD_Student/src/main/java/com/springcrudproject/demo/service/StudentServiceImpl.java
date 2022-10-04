e489a3297ee2d1eb10658f2a7d0c4fbebb50f069
Ravikiran Raghunath Kapse <114147319+Ravikiran5926@users.noreply.github.com>
2022-10-04 15:41:13 +0530

    Update StudentServiceImpl.java

--- SpringBoot_RESTful_Project/BackEnd_Project/BackEnd_CRUD_Student/src/main/java/com/springcrudproject/demo/service/StudentServiceImpl.java
+++ SpringBoot_RESTful_Project/BackEnd_Project/BackEnd_CRUD_Student/src/main/java/com/springcrudproject/demo/service/StudentServiceImpl.java
@@ -6,14 +6,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 
 import com.springcrudproject.demo.model.Student;
 import com.springcrudproject.demo.repository.StudentRepository;
 
 @Service
-@Transactional
 public class StudentServiceImpl implements StudentService
 {
 	@Autowired
